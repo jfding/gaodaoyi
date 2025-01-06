1ba1b2ed41b815fb34b8d1be9d132114d4cbc90f
JF Ding <jfding@gmail.com>
2025-01-06 18:57:34 +0800

    cli args can specify the three numbers directly

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use clap::Parser;
 use viuer::Config;
+use anyhow::Result;
 use image;
 
 // Embed the image data directly into the binary
-const ICON_DATA: &[u8] = include_bytes!("../assets/images/icon.png");
+const ICON_DATA: &[u8] = include_bytes!("../assets/images/book-cover.jpg");
+
+#[derive(Default, Debug)]
+struct Keys {
+    up: u8,
+    down: u8,
+    yao: u8,
+}
 
 /// Simple program to greet a person
 #[derive(Parser)]
@@ -22,10 +30,9 @@ struct Args {
     yao: Option<u8>,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn welcome_pic() -> Result<()> {
     // Load the embedded image data
     let img = image::load_from_memory(ICON_DATA)?;
-    println!("Image dimensions: {}x{}", img.width(), img.height());
 
     // Configure how to display the image
     let conf = Config {
@@ -44,38 +51,58 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Display the image
     viuer::print(&img, &conf)?;
 
-    let args = Args::parse();
+    Ok(())
+}
 
-    println!("up {:?}!", args.up);
-    println!("down {:?}!", args.down);
-    println!("yao {:?}!", args.yao);
+fn main() -> Result<()> {
+    //welcome_pic();
 
-    let up = args.up.unwrap_or_else(|| {
-        inquire::Select::new("Select up gua", vec!["1", "2", "3", "4", "5", "6", "7", "8"])
-            .prompt()
-            .expect("Failed to get user input")
-            .parse::<u8>()
-            .expect("Failed to parse input")
-    });
-    println!("up: {}", up);
+    let mut keys = Keys::default();
 
-    let down = args.down.unwrap_or_else(|| {
-        inquire::Select::new("Select down gua", vec!["1", "2", "3", "4", "5", "6", "7", "8"])
-            .prompt()
-            .expect("Failed to get user input")
-            .parse::<u8>()
-            .expect("Failed to parse input")
-    });
-    println!("down: {}", down);
+    // peek the cli args before parsing them
+    let ori_args :Vec<_> = std::env::args().collect();
 
-    let yao = args.yao.unwrap_or_else(|| {
-        inquire::Select::new("Select yao number", vec!["1", "2", "3", "4", "5", "6"])
-            .prompt()
-            .expect("Failed to get user input")
-            .parse::<u8>()
-            .expect("Failed to parse input")
-    });
-    println!("yao: {}", yao);
+    if ori_args.len() == 4 && ori_args.iter().skip(1).all(|arg| arg.parse::<u8>().is_ok()) {
+        // All arguments are valid numbers
+        let numbers :Vec<_> = ori_args.iter().skip(1).map(|arg| arg.parse::<u8>().unwrap()).collect();
+        keys.up  = numbers[0];
+        keys.down = numbers[1];
+        keys.yao = numbers[2];
+
+    } else {
+        let args = Args::parse();
+
+        let up = args.up.unwrap_or_else(|| {
+            inquire::Select::new("Select up gua", vec!["1", "2", "3", "4", "5", "6", "7", "8"])
+                .prompt()
+                .expect("Failed to get user input")
+                .parse::<u8>()
+                .expect("Failed to parse input")
+        });
+        println!("up: {}", up);
+
+        let down = args.down.unwrap_or_else(|| {
+            inquire::Select::new("Select down gua", vec!["1", "2", "3", "4", "5", "6", "7", "8"])
+                .prompt()
+                .expect("Failed to get user input")
+                .parse::<u8>()
+                .expect("Failed to parse input")
+        });
+        println!("down: {}", down);
+
+        let yao = args.yao.unwrap_or_else(|| {
+            inquire::Select::new("Select yao number", vec!["1", "2", "3", "4", "5", "6"])
+                .prompt()
+                .expect("Failed to get user input")
+                .parse::<u8>()
+                .expect("Failed to parse input")
+        });
+        println!("yao: {}", yao);
+
+        keys = Keys { up, down, yao };
+    }
+
+    println!("{:?}", keys);
 
     Ok(())
 }
